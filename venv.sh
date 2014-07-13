690a5c0cbcc298c81ddc563b1138fc3d875a66c1
Nico Revin <whois@nicorevin.ru>
2014-07-13 18:05:29 +0400

    Ensure we do not shoot(self.foot)

--- venv.sh
+++ venv.sh
@@ -56,10 +56,33 @@ function venv {
             shift
             if [ $# -gt 0 ]; then
                 for d in $*; do
-                    rm -r $VENV_HOME/$d && echo 'Virtualenv '$d' removed'
+                    if [ -d $VENV_HOME/$d ]; then
+                        if [ "$VENV_HOME/$d" = "$VIRTUAL_ENV" ]; then
+                            echo "Virtualenv $d is currently active."
+                            echo "Do you really want ot remove it? [yes/no]:"
+                            read VENV_RM
+                            case $VENV_RM in
+                                (y(|e(s|p)))
+                                    venv off
+                                    ;;
+                                (n(|o(|pe)))
+                                    continue
+                                    ;;
+                                *)
+                                    echo "Wrong choice"
+                                    continue
+                                    ;;
+                            esac
+                        fi
+                        rm -r $VENV_HOME/$d &>/dev/null && echo 'Virtualenv '$d' removed'
+                    else
+                        echo 'No such venv '$d
+                        return 1
+                    fi
                 done
             else
                 echo 'You must provide at least one virtualenv name to remove it'
+                return 1
             fi
             ;;
         *)
